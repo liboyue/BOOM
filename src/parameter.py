feb7bc17f4bc784b53d2f9d529bb1df80681084f
Boyue Li <me@boyue.li>
2018-02-14 21:46:26 -0500

    Added parameter class.

--- src/parameter.py
+++ src/parameter.py
@@ -1,20 +1,43 @@
 class Parameter:
-    """Parameter class."""
+    """This class."""
 
     def __init__(self, conf):
-        pass
+        ## Name of the parameter.
+        self.name = conf['name']
+        ## Starting value of the parameter.
+        self.start = conf['start']
+        ## Ending value of the parameter.
+        self.end = conf['end']
+        ## Step size for each update.
+        self.step_size = conf['step size']
+        ## Value of the parameter.
+        self.val = self.start
 
+    def __str__(self):
+        return 'name: ' + str(self.name) \
+                + ', start: ' + str(self.start) \
+                + ', end: ' + str(self.end) \
+                + ', step size:' + str(self.step_size)
+
+    ## Return current value.
     def get_value(self):
-        pass
+        return self.val
 
-    def set_value(self):
-        pass
+    ## Return current value.
+    #  @param val New value for the parameter.
+    def set_value(self, val):
+        self.val = val
 
-    def get_span(self):
-        pass
-
+    ## Update the parameter to the next value.
+    #  @return True if successfully updated, False if the next value is invalid.
     def next_value(self):
-        pass
+        if self.val + self.step_size <= self.end:
+            self.val += self.step_size
+            return True
+        else:
+            return False
 
+
+    ## Reset the parameter to the starting value.
     def reset_value(self):
-        pass
+        self.val = self.start
