d7ad4bd353875c9b41ab132a0180098b0e478282
Boyue Li <me@boyue.li>
2018-03-07 12:41:33 -0500

    Added setup.py and updated Makefile.

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-.PHONY: test cleantest
+.PHONY: all, install, test, doc
 
-export PYTHONPATH := ./:$(PYTHONPATH)
+all:
+	make install
+	make test
+	#make doc
 
+install:
+	python setup.py install
 
 test:
 	py.test --verbose --color=yes tests
 
 doc:
 	bash scripts/create_docs.sh
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='src',
+      version='0.0.0',
+      description='An easy-to-use question answering pipeline framework.',
+      author='Boyue & Nico',
+      author_email='boyuel@andrew.cmu.edu',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['src', 'src.modules']
+     )
