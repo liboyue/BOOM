34985e6f90d1681a55c385b9c16b2524915c3e0f
Boyue Li <me@boyue.li>
2018-03-26 22:57:39 -0400

    Optimized CoreMMR and SimilarityJaccard, almost 2x faster.

--- src/modules/bioasq/SimilarityJaccard.py
+++ src/modules/bioasq/SimilarityJaccard.py
@@ -3,10 +3,18 @@ from nltk.tokenize import word_tokenize
 
 
 class SimilarityJaccard(SimilarityMeasure):
-	def calculateSimilarity(self):
-		set1 = set([i.lower() for i in word_tokenize(self.sentence1) if i.lower() not in self.stopWords])
-		set2 = set([i.lower() for i in word_tokenize(self.sentence2) if i.lower() not in self.stopWords])
-		return float(len(set1.intersection(set2)))/len(set1.union(set2))
+    def __init__(self):
+        super().__init__('', '')
+
+    def calculateSimilarity(self, s1, s2):
+        s1 = word_tokenize(s1.lower())
+        s2 = word_tokenize(s2.lower())
+        #s1_u_s2 = set()
+        #s1_i_s2 = set()
+        set1 = {i for i in s1 if i not in self.stopWords}
+        set2 = {i for i in s2 if i not in self.stopWords}
+        self.score = len(set1.intersection(set2)) / len(set1.union(set2))
+        return self.score
 
 """
 instance = SimilarityJaccard("apple banana cat dog", "apple elephant cat dog")
