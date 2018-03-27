811fb1a75d524f20892ed58450f4f285eefe66bc
Boyue Li <me@boyue.li>
2018-03-26 22:57:09 -0400

    Bug fix for CoreMMR.

--- src/modules/mmr.py
+++ src/modules/mmr.py
@@ -12,10 +12,14 @@ class CoreMMR(Module):
 
         result = []
         for i, question in enumerate(data['questions']):
+            #log.info(i/len(data['questions']))
             #question['snippets'] = question['contexts']['long_snippets']
             #answer = question['ideal_answers'][0]
-            question['snippets'] = list(map(lambda x: x['text'], question['snippets']))
-            result.append(self.ranker.getRankedList(question, job.params['alpha']/100, 0))
+            #log.warn(str(i) + ", " + str(question))
+            if 'snippets' in question:
+                question['snippets'] = [s['text'] for s in question['snippets']]
+                result.append(self.ranker.getRankedList(question, job.params['alpha']/100, 0))
+
         log.debug(result)
 
         return result
