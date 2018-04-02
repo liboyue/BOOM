4bc3c611e464c9e6f91e6e2017a55d2bcbd9461d
Boyue Li <me@boyue.li>
2018-04-02 18:56:16 -0400

    Changed ranker to multiprocess version.

--- src/modules/mmr.py
+++ src/modules/mmr.py
@@ -1,26 +1,43 @@
 import glog as log
+from multiprocessing import Pool
+
 from .module import Module
-from .bioasq.coreMMR import CoreMMR as bioMMR
+from .bioasq.coreMMR import CoreMMR as BioASQCoreMMR
+
+def multi_process_helper(args):
+    questions, alpha = args
+    ranker = BioASQCoreMMR()
+    result = []
+    for question in questions:
+        if 'snippets' in question:
+            question['snippets'] = [s['text'] for s in question['snippets']]
+            result.append(ranker.getRankedList(question, alpha, 0))
+    log.debug(result)
+    return(result)
 
 class CoreMMR(Module):
 
     def __init__(self, module_id, name, rabbitmq_host, pipeline_conf, module_conf, **kwargs):
         super().__init__(module_id, name, rabbitmq_host, pipeline_conf, module_conf, **kwargs)
-        self.ranker = bioMMR()
+        self.processes = module_conf['processes'] if 'processes' in module_conf else 1
+        self.pool = Pool(processes=self.processes)
+
+    ## Override the cleanup function to make sure close the process pool.
+    def cleanup(self):
+        self.pool.close()
+        self.pool.join()
 
     def process(self, job, data):
 
+        questions = data['questions']
+        N = len(questions)
+        step_size = int(N / self.processes)
+        slices = [(questions[i:i+step_size], job.params['alpha']/100) for i in range(0, N, step_size)]
+        tmp = self.pool.map(multi_process_helper, slices)
+        
         result = []
-        for i, question in enumerate(data['questions']):
-            #log.info(i/len(data['questions']))
-            #question['snippets'] = question['contexts']['long_snippets']
-            #answer = question['ideal_answers'][0]
-            #log.warn(str(i) + ", " + str(question))
-            if 'snippets' in question:
-                question['snippets'] = [s['text'] for s in question['snippets']]
-                result.append(self.ranker.getRankedList(question, job.params['alpha']/100, 0))
-
-        log.debug(result)
+        for x in tmp:
+            result += x
 
         return result
 
--- stop_services.sh
+++ stop_services.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rabbitmq rabbitmqctl stop
+rabbitmqctl stop
 killall mongod
